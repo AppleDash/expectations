788bc6d14aef859a5188cccfefe2a7aba42ef7b9
AppleDash <pet@feather.horse>
2019-02-28 09:40:03 -0500

    One hundred percent code coverage!

--- src/test/java/org/appledash/expectations/tests/SimpleEqualityExpectorTest.java
+++ src/test/java/org/appledash/expectations/tests/SimpleEqualityExpectorTest.java
@@ -1,23 +1,24 @@
-package org.appledash.expectations;
+package org.appledash.expectations.tests;
 
 import org.appledash.expectations.expectors.type.RegularExpressionExpector;
+import org.appledash.expectations.expectors.type.SimpleEqualityExpector;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class RegularExpressionExpectorTest {
-    private RegularExpressionExpector<Integer> expector;
+public class SimpleEqualityExpectorTest {
+    private SimpleEqualityExpector<String, Integer> expector;
 
     @BeforeEach
     public void initializeExpector() {
-        this.expector = new RegularExpressionExpector<>();
+        this.expector = new SimpleEqualityExpector<>();
     }
 
     @Test
     public void testExpector() {
         int[] output = { 0 };
 
-        this.expector.expect("^[0-9]{3}$", data -> {
+        this.expector.expect("123", data -> {
             output[0] = data;
         });
 
